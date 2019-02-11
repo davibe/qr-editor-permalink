7927f393ff36f47a151e0e4e0c9112e975e56756
Davide Bertola <dade@dadeb.it>
2019-02-11 12:20:36 +0100

    Updates

---
+++ precache-manifest.7b07e8a2ae1b53b9144a03c1e47e22ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a2ac7dc78db091433e8",
+    "url": "/qr-editor-permalink/static/css/main.626f29e3.chunk.css"
+  },
+  {
+    "revision": "1a2ac7dc78db091433e8",
+    "url": "/qr-editor-permalink/static/js/main.1a2ac7dc.chunk.js"
+  },
+  {
+    "revision": "d8803bb799fdb86a5866",
+    "url": "/qr-editor-permalink/static/js/runtime~main.d8803bb7.js"
+  },
+  {
+    "revision": "7ffd0bb1f8b87358750b",
+    "url": "/qr-editor-permalink/static/js/2.7ffd0bb1.chunk.js"
+  },
+  {
+    "revision": "4f19f5c1853c37f092a3cf457121c325",
+    "url": "/qr-editor-permalink/index.html"
+  }
+];
